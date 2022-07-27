e2f28b634f352ce007b29890fc9bfbcff159f55c
kang.lee <dooully@gmail.com>
2022-07-27 14:58:17 +0900

    ui customizing

--- lib/embedded_tweet_view.dart
+++ lib/embedded_tweet_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:tweet_ui/default_text_styles.dart';
-import 'package:tweet_ui/models/api/v1/tweet.dart';
-import 'package:tweet_ui/models/api/v2/tweet_v2.dart';
+import 'package:tweet_ui/models/api/tweet.dart';
 import 'package:tweet_ui/models/viewmodels/tweet_vm.dart';
 import 'package:tweet_ui/on_tap_image.dart';
 import 'package:tweet_ui/src/byline.dart';
@@ -25,8 +24,9 @@ class EmbeddedTweetView extends StatelessWidget {
   /// If set to true the the text and icons will be light
   final bool darkMode;
 
-  /// If set to true, the number of replies on a tweet will be displayed
-  final bool showRepliesCount;
+  /// If set to true a betterplayer will be used in a Tweet containing a video.
+  /// If set to false a image placeholder will he shown and a video will be played in a new page.
+  final bool useVideoPlayer;
 
   /// If the Tweet contains a video then an initial volume can be specified with a value between 0.0 and 1.0.
   final double? videoPlayerInitialVolume;
@@ -37,59 +37,31 @@ class EmbeddedTweetView extends StatelessWidget {
   /// Date format when the tweet was created. When null it defaults to DateFormat("HH:mm • MM.dd.yyyy", 'en_US')
   final DateFormat? createdDateDisplayFormat;
 
-  /// If set to true betterplayer will load the highest quality available.
-  /// If set to false betterplayer will load the lowest quality available.
+  /// If set to true betterplayer/video_player will load the highest quality available.
+  /// If set to false betterplayer/video_player will load the lowest quality available.
   final bool videoHighQuality;
-
-  /// If set to true the video in the tweet, if available, will autoplay
-  /// By default it is false
-  final bool? autoPlayVideo;
-
-  /// If set to false will disallow user to enter full screen in tweet video
-  /// By default it is true
-  final bool? enableVideoFullscreen;
-
-  bool get _shouldShowReplies =>
-      showRepliesCount && _tweetVM.repliesCount != null;
 
   EmbeddedTweetView(
     this._tweetVM, {
     this.backgroundColor,
     required this.darkMode,
+    required this.useVideoPlayer,
     this.videoPlayerInitialVolume,
     this.onTapImage,
     this.createdDateDisplayFormat,
     required this.videoHighQuality,
-    this.showRepliesCount = false,
-    this.autoPlayVideo,
-    this.enableVideoFullscreen,
   }); //  TweetView(this.tweetVM);
 
-  EmbeddedTweetView.fromTweetV1(
-    TweetV1Response tweet, {
+  EmbeddedTweetView.fromTweet(
+    Tweet tweet, {
     this.backgroundColor = Colors.white,
     this.darkMode = false,
+    this.useVideoPlayer = true,
     this.videoPlayerInitialVolume = 0.0,
     this.onTapImage,
     this.createdDateDisplayFormat,
     this.videoHighQuality = true,
-    this.showRepliesCount = false,
-    this.autoPlayVideo,
-    this.enableVideoFullscreen,
   }) : _tweetVM = TweetVM.fromApiModel(tweet, createdDateDisplayFormat);
-
-  EmbeddedTweetView.fromTweetV2(
-    TweetV2Response tweet, {
-    this.backgroundColor = Colors.white,
-    this.darkMode = false,
-    this.videoPlayerInitialVolume = 0.0,
-    this.onTapImage,
-    this.createdDateDisplayFormat,
-    this.videoHighQuality = true,
-    this.showRepliesCount = false,
-    this.autoPlayVideo,
-    this.enableVideoFullscreen,
-  }) : _tweetVM = TweetVM.fromApiV2Model(tweet, createdDateDisplayFormat);
 
   @override
   Widget build(BuildContext context) {
@@ -124,8 +96,7 @@ class EmbeddedTweetView extends StatelessWidget {
                                 Padding(
                                   child: RetweetInformation(
                                     _tweetVM,
-                                    retweetInformationStyle:
-                                        defaultEmbeddedRetweetInformationStyle,
+                                    retweetInformationStyle: defaultEmbeddedRetweetInformationStyle,
                                   ),
                                   padding: EdgeInsets.only(left: 28),
                                 ),
@@ -134,22 +105,18 @@ class EmbeddedTweetView extends StatelessWidget {
                                     ProfileImage(tweetVM: _tweetVM),
                                     Expanded(
                                       child: Padding(
-                                        padding:
-                                            const EdgeInsets.only(left: 8.0),
+                                        padding: const EdgeInsets.only(left: 8.0),
                                         child: Byline(
                                           _tweetVM,
                                           ViewMode.standard,
                                           userNameStyle: TextStyle(
-                                            color: (darkMode)
-                                                ? Colors.white
-                                                : Colors.black,
+                                            color: (darkMode) ? Colors.white : Colors.black,
                                             fontSize: 16.0,
                                             fontFamily: 'Roboto',
                                             fontWeight: FontWeight.w700,
                                           ),
                                           showDate: false,
-                                          userScreenNameStyle:
-                                              defaultEmbeddedUserNameStyle,
+                                          userScreenNameStyle: defaultEmbeddedUserNameStyle,
                                         ),
                                       ),
                                     ),
@@ -172,33 +139,23 @@ class EmbeddedTweetView extends StatelessWidget {
                     },
                     child: TweetText(
                       _tweetVM,
-                      textStyle: (darkMode)
-                          ? defaultEmbeddedDarkTextStyle
-                          : defaultEmbeddedTextStyle,
+                      textStyle: (darkMode) ? defaultEmbeddedDarkTextStyle : defaultEmbeddedTextStyle,
                       clickableTextStyle: defaultEmbeddedClickableTextStyle,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8.0, vertical: 15.0),
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 15.0),
                     ),
                   ),
                   (_tweetVM.quotedTweet != null)
                       ? Padding(
                           padding: EdgeInsets.only(top: 8.0, bottom: 10),
-                          child: QuoteTweetViewEmbedded(
+                          child: QuoteTweetViewEmbed.fromTweet(
                             _tweetVM.quotedTweet!,
-                            textStyle: TextStyle(
-                                color:
-                                    (darkMode) ? Colors.white : Colors.black),
+                            textStyle: TextStyle(color: (darkMode) ? Colors.white : Colors.black),
                             clickableTextStyle: defaultQuoteClickableTextStyle,
-                            userNameStyle: (darkMode)
-                                ? defaultEmbeddedDarkQuoteUserNameStyle
-                                : defaultQuoteUserNameStyle,
-                            userScreenNameStyle:
-                                defaultQuoteUserScreenNameStyle,
+                            userNameStyle: (darkMode) ? defaultEmbeddedDarkQuoteUserNameStyle : defaultQuoteUserNameStyle,
+                            userScreenNameStyle: defaultQuoteUserScreenNameStyle,
                             backgroundColor: null,
                             borderColor: null,
                             onTapImage: onTapImage,
-                            autoPlayVideo: autoPlayVideo,
-                            enableVideoFullscreen: enableVideoFullscreen,
                           ),
                         )
                       : Container(),
@@ -211,12 +168,18 @@ class EmbeddedTweetView extends StatelessWidget {
             child: MediaContainer(
               _tweetVM,
               ViewMode.standard,
-              videoPlayerInitialVolume: videoPlayerInitialVolume,
+              useVideoPlayer: useVideoPlayer,
+              videoPlayerInitialVolume: videoPlayerInitialVolume ?? 0.0,
               onTapImage: onTapImage,
               videoHighQuality: videoHighQuality,
-              autoPlayVideo: autoPlayVideo,
-              enableVideoFullscreen: enableVideoFullscreen,
             ),
+          ),
+          Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.only(top: 5, left: 20, right: 20, bottom: 5),
+              child: Text(_tweetVM.createdAt, style: TextStyle(color: (darkMode) ? Colors.grey[400] : Colors.grey[600]))),
+          Divider(
+            color: Colors.grey[400],
           ),
           Container(
             margin: EdgeInsets.only(top: 5, left: 20, right: 20, bottom: 5),
@@ -228,84 +191,59 @@ class EmbeddedTweetView extends StatelessWidget {
                   size: 18,
                 ),
                 Container(
-                    margin: EdgeInsets.only(left: 6),
-                    child: Text(_tweetVM.favoriteCount.toString(),
-                        style: TextStyle(
-                            color: (darkMode)
-                                ? Colors.grey[400]
-                                : Colors.grey[600]))),
-                if (_shouldShowReplies)
-                  Container(
-                    margin: EdgeInsets.only(left: 16),
-                    child: Icon(
-                      _tweetVM.replied
-                          ? Icons.mode_comment
-                          : Icons.mode_comment_outlined,
-                      color: (darkMode) ? Colors.grey[400] : Colors.grey[600],
-                      size: 18,
-                    ),
-                  ),
-                if (_shouldShowReplies)
-                  Container(
-                      margin: EdgeInsets.only(left: 6),
-                      child: Text(_tweetVM.repliesCount.toString(),
-                          style: TextStyle(
-                              color: (darkMode)
-                                  ? Colors.grey[400]
-                                  : Colors.grey[600]))),
-                if (_tweetVM.createdAt != null)
-                  Container(
-                    margin: EdgeInsets.only(left: 16),
-                    child: Text(
-                      _tweetVM.createdAt!,
-                      style: TextStyle(
-                          color:
-                              (darkMode) ? Colors.grey[400] : Colors.grey[600]),
-                    ),
-                  )
+                    margin: EdgeInsets.only(left: 6, right: 10),
+                    child: Text(_tweetVM.favoriteCount.toString(), style: TextStyle(color: (darkMode) ? Colors.grey[400] : Colors.grey[600]))),
+                Image.asset(
+                  "assets/tw__ic_retweet_light.png",
+                  fit: BoxFit.fitWidth,
+                  package: 'tweet_ui',
+                  color: (darkMode) ? Colors.grey[400] : Colors.grey[600],
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 6, right: 10),
+                    child: Text(_tweetVM.retweetCount.toString(), style: TextStyle(color: (darkMode) ? Colors.grey[400] : Colors.grey[600]))),
               ],
             ),
           ),
-          if (_tweetVM.userName.isNotEmpty)
-            Divider(
-              color: Colors.grey[400],
-            ),
-          if (_tweetVM.userName.isNotEmpty)
-            Container(
-              margin: EdgeInsets.only(left: 20, right: 20, bottom: 15, top: 5),
-              child: GestureDetector(
-                behavior: HitTestBehavior.translucent,
-                onTap: () {
-                  openUrl(_tweetVM.userLink);
-                },
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.person_outline,
-                      color: (darkMode) ? Colors.blue[100] : Colors.blue[700],
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: Text(
-                          "${_tweetVM.userName}'s other tweets",
-                          style: TextStyle(
-                              color: (darkMode)
-                                  ? Colors.blue[100]
-                                  : Colors.blue[800],
-                              fontWeight: FontWeight.w400),
-                          maxLines: 1,
-                          overflow: TextOverflow.fade,
-                          softWrap: false,
-                          textAlign: TextAlign.start,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
+          SizedBox.fromSize(size: Size.fromHeight(10))
+          // Divider(
+          //   color: Colors.grey[400],
+          // ),
+          // Container(
+          //   margin: EdgeInsets.only(left: 20, right: 20, bottom: 15, top: 5),
+          //   child: GestureDetector(
+          //     behavior: HitTestBehavior.translucent,
+          //     onTap: () {
+          //       openUrl(_tweetVM.userLink);
+          //     },
+          //     child: Row(
+          //       crossAxisAlignment: CrossAxisAlignment.center,
+          //       children: [
+          //         Icon(
+          //           Icons.person_outline,
+          //           color: (darkMode) ? Colors.blue[100] : Colors.blue[700],
+          //         ),
+          //         Expanded(
+          //           child: Padding(
+          //             padding: const EdgeInsets.only(left: 5),
+          //             child: Text(
+          //               "${_tweetVM.userName}'s other tweets",
+          //               style: TextStyle(
+          //                   color: (darkMode)
+          //                       ? Colors.blue[100]
+          //                       : Colors.blue[800],
+          //                   fontWeight: FontWeight.w400),
+          //               maxLines: 1,
+          //               overflow: TextOverflow.fade,
+          //               softWrap: false,
+          //               textAlign: TextAlign.start,
+          //             ),
+          //           ),
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          // )
         ],
       ),
     );
